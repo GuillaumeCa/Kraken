501bd3350a16832f0cecf914a336a7d7f1334074
GuillaumeCa <guillaume.carre@isep.fr>
2017-03-22 09:17:40 +0100

    test ldap

---
+++ gcfa-back/src/main/java/com/kraken/gcfa/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.kraken.gcfa.controller;
+
+import com.kraken.gcfa.dto.LDAPUserDTO;
+import com.kraken.gcfa.services.LDAPService;
+import com.kraken.gcfa.services.LDAPServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Guillaume on 22/03/2017.
+ * gcfa-back
+ */
+@RequestMapping(value = "/login")
+@RestController
+public class LoginController {
+
+    @Autowired
+    private LDAPService ldapService;
+
+    @RequestMapping(value = "/{user}/{pwd}")
+    public LDAPUserDTO getUser(@PathVariable String user, @PathVariable String pwd) throws LDAPServiceException {
+        return ldapService.getUser(user, pwd);
+    }
+}
