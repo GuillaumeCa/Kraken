594bc649d41535b75f6a796ca66f8fc7232f89a9
Arfaz <aferoz@hotmail.fr>
2017-03-22 13:48:16 +0100

    Correction du bug de l’appel à la fonction Hello

--- gcfa-front/src/App.js
+++ gcfa-front/src/App.js
@@ -16,7 +16,6 @@ class App extends Component {
         <Test title="Test ghhhg">
           fgfghfghfhg
         </Test>
-        <Test.Hello />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
