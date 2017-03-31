1fb60a5cc53f4883337d7f9b239cb3787bb5c903
Arfaz <aferoz@hotmail.fr>
2017-04-01 00:35:08 +0200

    Test de Création du formulaire de Login

--- gcfa-front/src/pages/Login.js
+++ gcfa-front/src/pages/Login.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+import TextField from 'material-ui/TextField';
 
 
 class Login extends Component {
   render() {
     return (
-      <div>
+      		<div>
+              <TextField hintText="Login" />
+              <br />
+
+              <TextField ref='password'
+                  hintText="Password"
+                  fieldName="p"
+                  errorText="This field is required."
+                  type="password">
+              </TextField>
+              <br />
+
+              <RaisedButton label="Valider" backgroundColor="#a4c639" />
+              
+            </div>
         
-      </div>
     );
   }
 }
