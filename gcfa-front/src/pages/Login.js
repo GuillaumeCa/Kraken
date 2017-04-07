6d13b8a881fe4eaee51dcee784d22c88199aa5de
GuillaumeCa <guillaume.carre@isep.fr>
2017-04-08 00:32:28 +0200

    better looking login page :p

--- gcfa-front/src/pages/Login.js
+++ gcfa-front/src/pages/Login.js
@@ -3,35 +3,50 @@ import React, { Component } from 'react';
 import FlatButton from 'material-ui/FlatButton';
 import {Card, CardActions, CardText, CardHeader} from 'material-ui/Card';
 import TextField from 'material-ui/TextField';
+import ActionLockOpen from 'material-ui/svg-icons/action/lock-open';
+
+const BACKGROUND_STYLE = {
+  height: '100%',
+  background: 'url(background.jpg)',
+  backgroundRepeat: 'none',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center'
+}
 
 const LOGIN_STYLE = {
+  position: 'relative',
   maxWidth: 400,
   margin: '0 auto',
-  marginTop: '10%',
+  top: '10%',
 }
 
 class Login extends Component {
   render() {
     const error = false;
     return (
-  		<div>
-        <Card style={LOGIN_STYLE}>
+  		<div style={BACKGROUND_STYLE}>
+        <Card style={LOGIN_STYLE} zDepth={3}>
           <CardText>
             <h1 className="primary-color">GCFA</h1>
             <TextField
-              hintText="Login"
+              hintText="Nom d'utilisateur"
               fullWidth
             />
             <TextField
               type="password"
-              hintText="Password"
+              hintText="Mot de passe"
               errorText={error && "This field is required."}
               fullWidth
             />
           </CardText>
-          <CardActions>
-            <FlatButton primary label="Connexion" />
-            <FlatButton href="http://moncompte.isep.fr" secondary label="Mot de passe oublié" />
+          <CardActions style={{ textAlign: 'right' }}>
+            <FlatButton href="http://moncompte.isep.fr" label="Mot de passe oublié" />
+            <FlatButton
+              secondary
+              label="Connexion"
+              labelPosition="before"
+              icon={<ActionLockOpen />}
+            />
           </CardActions>
         </Card>
       </div>
