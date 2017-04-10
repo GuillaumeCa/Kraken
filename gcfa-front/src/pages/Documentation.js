5c8e64635b2916964c668e71b05f335690da3b5c
Arfaz <aferoz@hotmail.fr>
2017-04-10 23:56:23 +0200

    [Front] Ajout des BarCard sur la page documentation

--- gcfa-front/src/pages/Documentation.js
+++ gcfa-front/src/pages/Documentation.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
 
+import FlatButton from 'material-ui/FlatButton';
+
+import BarCard, { DocumentCard } from '../components/BarCard';
+
+const BUTTON_STYLE = {
+  fontSize: 20,
+}
+
+
 class Documentation extends Component {
   render() {
+  	const actions = [
+  		<FlatButton primary label="Voir" labelStyle={BUTTON_STYLE} />,
+	]
     return (
-      <div>
-        <h1 className="main-title">Documentation</h1>
-      </div>
+    	<div>
+	      <div>
+	        <h1 className="main-title">Documentation</h1>
+	      </div>
+
+	      <section>
+		    <h2 className="sub-title">Calendriers</h2>
+		    <BarCard actions={actions}>
+		      <DocumentCard title="Calendrier A3 (Promo 2017)" />
+		    </BarCard>
+		    <BarCard actions={actions}>
+		      <DocumentCard title="Calendrier A2 (Promo 2018)" />
+		    </BarCard>
+		    <BarCard actions={actions}>
+		      <DocumentCard title="Calendrier A1 (Promo 2019)" />
+		    </BarCard>
+		  </section>
+
+	      <section>
+	        <h2 className="sub-title">Outils de l'apprenti</h2>
+	        <BarCard actions={actions}>
+	          <DocumentCard title="Présentation de GCFA" />
+	        </BarCard>
+	        <BarCard actions={actions}>
+	          <DocumentCard title="Guide de l'alternance"/>
+	        </BarCard>
+	      </section>
+
+	      <section>
+	        <h2 className="sub-title">Documents d'évaluation</h2>
+	        <BarCard actions={actions}>
+	          <DocumentCard title="Évaluation de début d'alternance"/>
+	        </BarCard>
+	        <BarCard actions={actions}>
+	          <DocumentCard title="Rapport d'étape"/>
+	        </BarCard>
+	      </section>
+	    </div>
     );
   }
 }
