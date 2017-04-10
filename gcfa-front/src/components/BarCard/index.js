ac0206f5001e4106ed62b5dba21fd9bf0a742eb9
GuillaumeCa <guillaume.carre@isep.fr>
2017-04-10 22:46:53 +0200

    [front] new component for list of documents

---
+++ gcfa-front/src/components/BarCard/index.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import './BarCard.css';
+
+import colors from '../../colors';
+
+const BAR_STYLE = {
+  display: 'flex',
+  borderRadius: 5,
+  background: colors.GREY_LIGHT,
+  maxWidth: 600,
+  marginBottom: 10,
+}
+const ACTION_STYLE = {
+  display: 'flex',
+  marginLeft: 'auto',
+  alignItems: 'center',
+  marginRight: 10,
+}
+
+export default function BarCard(props) {
+  return (
+    <div style={BAR_STYLE}>
+      {props.children}
+      <div style={ACTION_STYLE}>
+        {props.actions}
+      </div>
+    </div>
+  )
+}
+
+const DOCUMENT_STYLE = {
+  padding: 10,
+}
+
+const DOCUMENT_TITLE_STYLE = {
+  fontSize: 25,
+  fontWeight: 'normal',
+  color: colors.GREY_DARK,
+  margin: 0,
+  marginBottom: 5,
+}
+const DOCUMENT_SUBTITLE_STYLE = {
+  margin: 0,
+  color: colors.GREY_DARK,
+  fontStyle: 'italic',
+}
+
+export function DocumentCard(props) {
+  return (
+    <div style={DOCUMENT_STYLE}>
+      <h1 style={DOCUMENT_TITLE_STYLE}>{props.title}</h1>
+      <p style={DOCUMENT_SUBTITLE_STYLE}>{props.subtitle}</p>
+    </div>
+  )
+}
--- gcfa-front/src/pages/Home.js
+++ gcfa-front/src/pages/Home.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 
 import FlatButton from 'material-ui/FlatButton';
 
+import BarCard, { DocumentCard } from '../components/BarCard';
+
 const HEAD_STYLE = {
   display: 'flex',
   alignItems: 'center',
 }
 
+const BUTTON_STYLE = {
+  fontSize: 20,
+}
+
 class Home extends Component {
   render() {
+    const actions = [
+      <FlatButton primary label="Déposer" labelStyle={BUTTON_STYLE} />,
+    ]
     return (
       <div>
         <div style={HEAD_STYLE}>
@@ -17,6 +26,15 @@ class Home extends Component {
             <FlatButton primary label="Afficher tout" backgroundColor="#fff" hoverColor="#eee" />
           </div>
         </div>
+        <BarCard actions={actions}>
+          <DocumentCard title="Déclaration de compétences" subtitle="sous-titre" />
+        </BarCard>
+        <BarCard actions={actions}>
+          <DocumentCard title="Déclaration de compétences" subtitle="sous-titre" />
+        </BarCard>
+        <BarCard actions={actions}>
+          <DocumentCard title="Déclaration de compétences" subtitle="sous-titre" />
+        </BarCard>
       </div>
     );
   }
