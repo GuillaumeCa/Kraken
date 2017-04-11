83b7da5897a8bc11269a58001111baefc78e19d7
GuillaumeCa <guillaume.carre@isep.fr>
2017-04-11 19:08:23 +0200

    [front] clean modal

--- gcfa-front/src/pages/Home.js
+++ gcfa-front/src/pages/Home.js
@@ -14,38 +14,39 @@ const BUTTON_STYLE = {
   fontSize: 20,
 }
 
+const tempData = [
+  {
+    id: 1,
+    name: "Déclaration de compétences 1",
+  },
+  {
+    id: 2,
+    name: "Déclaration de compétences 2",
+  },
+  {
+    id: 3,
+    name: "Déclaration de compétences 3",
+  },
+]
+
 class Home extends Component {
-  constructor() {
-  	super()
-  	this.state = {
-  		openModal: false,
-  		modalTitle: "",
-  	}
+  state = {
+    openModal: false,
+    modalData: {},
   }
 
-  actions = (attributes) => {
-  	return (
-  		<FlatButton key={attributes.key} primary label="Déposer" labelStyle={BUTTON_STYLE} onTouchTap={() => {this.onClickButton(attributes)}} />
-  	)
-  }
-
-  onClickButton = (attributes) => {
-	const newState = {
-		openModal: true,
-		title: attributes.title,
-	}
-
-	this.setState(newState)
-
+  onClickButton = (data) => {
+  	this.setState({
+  		openModal: true,
+  		modalData: data,
+  	})
   }
 
   handleClose = () => {
-  	const newState = {
+  	this.setState({
   		openModal: false,
-  		title: "",
-  	}
-
-  	this.setState(newState)
+  		modalData: {},
+  	})
   }
 
   handleSubmit = () => {
@@ -56,17 +57,19 @@ class Home extends Component {
   render() {
 
   	const modalButtons = [
-	  <FlatButton
-	    label="Annuler"
-	    primary={true}
-	    onTouchTap={this.handleClose}
-	  />,
-	  <FlatButton
-	    label="Déposer"
-	    primary={true}
-	    onTouchTap={this.handleSubmit}
-	  />,
-	];
+  	  <FlatButton
+  	    label="Annuler"
+  	    primary={true}
+  	    onTouchTap={this.handleClose}
+  	  />,
+  	  <FlatButton
+  	    label="Déposer"
+  	    primary={true}
+  	    onTouchTap={this.handleSubmit}
+  	  />,
+  	];
+
+    const { modalData, openModal } = this.state;
 
     return (
       <div>
@@ -78,28 +81,42 @@ class Home extends Component {
         </div>
         <section>
           <h2 className="sub-title">Déposés</h2>
-          <BarCard actions={this.actions({key:1, title:"Déclaration de compétences" })}>
-            <DocumentCard title="Déclaration de compétences" subtitle="sous-titre" />
-          </BarCard>
-          <BarCard actions={this.actions({key:2, title:"Déclaration de compétences" })}>
-            <DocumentCard title="Déclaration de compétences" subtitle="sous-titre" />
-          </BarCard>
-          <BarCard actions={this.actions({key:3, title:"Déclaration de compétences" })}>
-            <DocumentCard title="Déclaration de compétences" subtitle="sous-titre" />
-          </BarCard>
+          {
+            tempData.map(data => {
+              return (
+                <BarCard key={data.id} actions={
+                    <FlatButton primary label="Déposer" labelStyle={BUTTON_STYLE}
+                      onTouchTap={() => this.onClickButton(data)}
+                    />
+                  }>
+                  <DocumentCard title={data.name} subtitle="sous-titre" />
+                </BarCard>
+              )
+            })
+          }
         </section>
         <section>
           <h2 className="sub-title">A venir</h2>
-          <BarCard actions={this.actions({key:4, title:"Déclaration de compétences" })}>
-            <DocumentCard title="Déclaration de compétences" subtitle="sous-titre" />
-          </BarCard>
+          {
+            tempData.map(data => {
+              return (
+                <BarCard key={data.id} actions={
+                    <FlatButton primary label="Déposer" labelStyle={BUTTON_STYLE}
+                      onTouchTap={() => this.onClickButton(data)}
+                    />
+                  }>
+                  <DocumentCard title={data.name} subtitle="sous-titre" />
+                </BarCard>
+              )
+            })
+          }
         </section>
 
         <UploadModal
         	title="Ajouter un document"
-        	open={this.state.openModal}
+        	open={openModal}
         	actions={modalButtons}
-        	docType={this.state.title}
+        	docType={modalData.name}
         	subtitle="A rendre avant le 10/03/2016"
         />
       </div>
