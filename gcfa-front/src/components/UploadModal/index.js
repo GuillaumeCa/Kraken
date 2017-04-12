11207d89a353981b71e9c458f267a032d590b579
GuillaumeCa <guillaume.carre@isep.fr>
2017-04-13 00:38:14 +0200

    [front] dropzone style

--- gcfa-front/src/components/UploadModal/index.js
+++ gcfa-front/src/components/UploadModal/index.js
@@ -5,6 +5,7 @@ import FlatButton from 'material-ui/FlatButton';
 import Dialog from 'material-ui/Dialog';
 
 import colors from '../../colors';
+import './UploadModal.css';
 
 const MODAL_TITLE_STYLE = {
   fontSize: 40,
@@ -29,6 +30,32 @@ const MODAL_SUBTITLE_STYLE = {
   color: colors.GREY_DARK,
 }
 
+const DROPZONE_STYLE = {
+  width: '100%',
+  background: colors.GREY_LIGHT,
+  color: colors.GREY_DARK,
+  borderRadius: 5,
+  padding: 40,
+  marginTop: 20,
+  textAlign: 'center',
+}
+
+const DROPZONE_ICON_STYLE = {
+  width: 60,
+  marginBottom: 10,
+}
+
+const DROPZONE_TITLE_STYLE = {
+  fontWeight: 'normal',
+  fontSize: 25,
+  margin: 0,
+  marginBottom: 10,
+}
+
+const DROPZONE_MSG_STYLE = {
+  fontSize: 15,
+  margin: 0,
+}
 
 export default class UploadModal extends Component {
   state = {
@@ -54,8 +81,10 @@ export default class UploadModal extends Component {
           <p style={MODAL_SUBTITLE_STYLE}>{this.props.subtitle}</p>
 
           <div>
-            <Dropzone ref={(node) => { this.dropzone = node; }} onDrop={this.onDrop}>
-                <div>Try dropping some files here, or click to select files to upload.</div>
+            <Dropzone ref={(node) => { this.dropzone = node; }} onDrop={this.onDrop} style={DROPZONE_STYLE} className="dropzone" activeClassName="dropzone-hover">
+              <img src="icons/download.png" alt="download-icon" style={DROPZONE_ICON_STYLE}/>
+              <h2 style={DROPZONE_TITLE_STYLE}>Déposez votre document ici</h2>
+              <p style={DROPZONE_MSG_STYLE}>PDF obligatoire (10 Mo maximum)</p>
             </Dropzone>
 
             {this.state.files.length > 0 ? <div>
