e0225c6426ab2a071d3379e9699b6543862d6d26
Arfaz <aferoz@hotmail.fr>
2017-04-12 16:37:16 +0200

    [Front] Intégration de la dropzone (sans validation ni css)

--- gcfa-front/src/components/UploadModal/index.js
+++ gcfa-front/src/components/UploadModal/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
 
 import FlatButton from 'material-ui/FlatButton';
 import Dialog from 'material-ui/Dialog';
@@ -28,45 +29,47 @@ const MODAL_SUBTITLE_STYLE = {
   color: colors.GREY_DARK,
 }
 
-const UPLOAD_BTN_STYLE = {
-  uploadButton: {
-    verticalAlign: 'middle',
-    width:'50%',
-    height:100
-  },
-  uploadInput: {
-    cursor: 'pointer',
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-    width: '100%',
-    opacity: 0,
-  },
-}
 
-export default function UploadModal(props) {
-  return (
-    <Dialog
-      modal={true}
-      open={props.open}
-      actions={props.actions}
-    >
-      <div style={MODAL_CONTAINER_STYLE}>
-        <h1 style={MODAL_TITLE_STYLE}>{props.title}</h1>
-        <h2 style={MODAL_DOCTYPE_STYLE}>{props.docType}</h2>
-        <p style={MODAL_SUBTITLE_STYLE}>{props.subtitle}</p>
-        <FlatButton
-          label="Importer"
-          labelPosition="before"
-          containerElement="label"
-          style={UPLOAD_BTN_STYLE.uploadButton}
-        >
-          <input type="file" style={UPLOAD_BTN_STYLE.uploadInput} />
-        </FlatButton>
+export default class UploadModal extends Component {
+  constructor() {
+    super()
+    this.state = {
+      files: [],
+    }
+  }
 
-      </div>
-    </Dialog>
-  )
+  onDrop= (acceptedFiles) => {
+    this.setState({
+      files: acceptedFiles
+    });
+  }
+
+  render() {
+    return (
+      <Dialog
+        modal={true}
+        open={this.props.open}
+        actions={this.props.actions}
+      >
+        <div style={MODAL_CONTAINER_STYLE}>
+          <h1 style={MODAL_TITLE_STYLE}>{this.props.title}</h1>
+          <h2 style={MODAL_DOCTYPE_STYLE}>{this.props.docType}</h2>
+          <p style={MODAL_SUBTITLE_STYLE}>{this.props.subtitle}</p>
+          
+          <div>
+            <Dropzone ref={(node) => { this.dropzone = node; }} onDrop={this.onDrop}>
+                <div>Try dropping some files here, or click to select files to upload.</div>
+            </Dropzone>
+         
+            {this.state.files.length > 0 ? <div>
+            <h2>{this.state.files.length} fichier séléctionné(s)</h2>
+            <div>{this.state.files.map((file) => <p>{file.name}</p> )}</div>
+            </div> : null}
+          </div>
+
+        </div>
+      </Dialog>
+  
+    )
+  }
 }
