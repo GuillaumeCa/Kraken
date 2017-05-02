3137379a1df99a146bcf81b85fa702b3b3f51e41
GuillaumeCa <guillaume.carre@isep.fr>
2017-05-02 17:25:19 +0200

    merge

--- gcfa-back/src/main/java/com/kraken/gcfa/controller/DocumentController.java
+++ gcfa-back/src/main/java/com/kraken/gcfa/controller/DocumentController.java
@@ -1,30 +1,19 @@
 package com.kraken.gcfa.controller;
 
-import java.io.File;
-
-import javax.annotation.security.RolesAllowed;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Role;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.MediaType;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.kraken.gcfa.constants.RolesNames;
-import com.kraken.gcfa.entity.DocumentationType;
 import com.kraken.gcfa.entity.User;
 import com.kraken.gcfa.exceptions.StorageException;
 import com.kraken.gcfa.services.DocumentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 
 @RestController
 @RequestMapping("/document")
@@ -57,7 +46,7 @@ public class DocumentController {
      */
     @GetMapping(value = "/{documentId}", produces = MediaType.APPLICATION_PDF_VALUE)
     public FileSystemResource getFile(@PathVariable Long documentId, HttpServletResponse response, @AuthenticationPrincipal User auth) throws StorageException {
-        File file = documentService.getFile(documentId,auth);
+        File file = documentService.getFile(documentId, auth);
         response.addHeader("Access-Control-Expose-Headers", "x-filename");
         response.addHeader("x-filename", file.getName());
         return new FileSystemResource(file);
@@ -66,12 +55,12 @@ public class DocumentController {
     /**
      * Supprimer une document
      *
-     * @param fileId
+     * @param documentId
      * @throws StorageException
      */
     @DeleteMapping("/{documentId}")
-    @RolesAllowed({RolesNames.SUPER_ADMIN,RolesNames.APPRENTICE})
+    @RolesAllowed({RolesNames.SUPER_ADMIN, RolesNames.APPRENTICE})
     public void deleteFile(@PathVariable Long documentId, @AuthenticationPrincipal User auth) throws StorageException {
-        documentService.deleteFile(documentId,auth);
+        documentService.deleteFile(documentId, auth);
     }
 }
