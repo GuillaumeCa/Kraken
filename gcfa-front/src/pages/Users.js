d113b5d0084f8bbc2a0584d055cd23845dd3c6a0
Arfaz <aferoz@hotmail.fr>
2017-05-22 21:37:07 +0200

    [front] creation de la page de gestions des users

---
+++ gcfa-front/src/pages/Users.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Drawer from 'material-ui/Drawer';
+
+
+class Users extends Component {
+  render() {
+    return (
+
+    	<Drawer
+
+    	/>
+    )
+  }
+
+}
+
+export default Users;
