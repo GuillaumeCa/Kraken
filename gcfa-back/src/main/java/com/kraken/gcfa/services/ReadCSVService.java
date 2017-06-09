62c15eeceb5acdfc858880c4cefd3a5949936ae6
romnar <romain.narboni@isep.fr>
2017-06-10 00:44:27 +0200

    [back] fix order of index in CSVService

--- gcfa-back/src/main/java/com/kraken/gcfa/services/ReadCSVService.java
+++ gcfa-back/src/main/java/com/kraken/gcfa/services/ReadCSVService.java
@@ -67,25 +67,25 @@ public class ReadCSVService {
 				user.setLastName(param[1]);
 				user.setRole(role);
 
-				user.setEmail(param[3]);
-				user.setSexe(param[4]);
+				user.setEmail(param[2]);
+				user.setSexe(param[3]);
 				apprentice.setUser(user);
 				users.add(user);
 
-				companySite = companySiteRepository.findByName(param[5]);
+				companySite = companySiteRepository.findByName(param[4]);
 				if(companySite == null) {
-					throw new NotFoundException(String.format("The CompanySite %s don't exist", param[5]));
+					throw new NotFoundException(String.format("The CompanySite %s don't exist", param[4]));
 				}
 				apprentice.setCompanySite(companySite);
-				apprentice.setPromotion(Integer.parseInt((param[6])));
-				if(param[7] == "3 ans") {
+				apprentice.setPromotion(Integer.parseInt((param[5])));
+				if(param[6] == "3 ans") {
 					apprentice.setContractType(ContractType.THREE_YEARS);
 				} else if(param[7] == "2 ans") {
 					apprentice.setContractType(ContractType.TWO_YEARS);
 				}
-				tutor = tutorRepository.findTutorByEmail(param[8]);
+				tutor = tutorRepository.findTutorByEmail(param[7]);
 				if(tutor == null) {
-					throw new NotFoundException(String.format("The Tutor with email %s doesn't exist", param[8]));
+					throw new NotFoundException(String.format("The Tutor with email %s doesn't exist", param[7]));
 				} 
 				apprentice.setTutor(tutor);
 					
