e037d51379e25d9c0b88097222cac21bf5a72dcb
romnar <romain.narboni@isep.fr>
2017-06-10 13:01:57 +0200

    [back] add ldap_id in the CSVService

--- gcfa-back/src/main/java/com/kraken/gcfa/services/ReadCSVService.java
+++ gcfa-back/src/main/java/com/kraken/gcfa/services/ReadCSVService.java
@@ -68,24 +68,25 @@ public class ReadCSVService {
 				user.setRole(role);
 
 				user.setEmail(param[2]);
-				user.setSexe(param[3]);
+				user.setLdapId(param[3]);
+				user.setSexe(param[4]);
 				apprentice.setUser(user);
 				users.add(user);
 
-				companySite = companySiteRepository.findByName(param[4]);
+				companySite = companySiteRepository.findByName(param[5]);
 				if(companySite == null) {
-					throw new NotFoundException(String.format("The CompanySite %s don't exist", param[4]));
+					throw new NotFoundException(String.format("The CompanySite %s don't exist", param[5]));
 				}
 				apprentice.setCompanySite(companySite);
-				apprentice.setPromotion(Integer.parseInt((param[5])));
-				if(param[6].equals("3 ans")) {
+				apprentice.setPromotion(Integer.parseInt((param[6])));
+				if(param[7].equals("3 ans")) {
 					apprentice.setContractType(ContractType.THREE_YEARS);
-				} else if(param[6].equals("2 ans")) {
+				} else if(param[7].equals("2 ans")) {
 					apprentice.setContractType(ContractType.TWO_YEARS);
 				}
-				tutor = tutorRepository.findTutorByEmail(param[7]);
+				tutor = tutorRepository.findTutorByEmail(param[8]);
 				if(tutor == null) {
-					throw new NotFoundException(String.format("The Tutor with email %s doesn't exist", param[7]));
+					throw new NotFoundException(String.format("The Tutor with email %s doesn't exist", param[8]));
 				} 
 				apprentice.setTutor(tutor);
 					
