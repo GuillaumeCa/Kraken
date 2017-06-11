c763bc7fab7b9b023341b9294da1216bc2ec5e15
GuillaumeCa <guillaume.carre@isep.fr>
2017-06-11 16:55:08 +0200

    fix failed merge :p

--- gcfa-front/src/components/UserDetails/Tutor/index.js
+++ gcfa-front/src/components/UserDetails/Tutor/index.js
@@ -117,5 +117,3 @@ export default class TutorDetail extends Component {
     )
   }
 }
-
-export default TutorDetail;
--- gcfa-front/src/pages/Tutors.js
+++ gcfa-front/src/pages/Tutors.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import { Link } from 'react-router-dom';
 
 import FlatButton from 'material-ui/FlatButton';
 
